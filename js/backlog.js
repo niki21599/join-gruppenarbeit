5e8df61ee829b29fd2600ee19f3af29e94f19213
Marco Riemann <91315420+misterreihmaehn@users.noreply.github.com>
2021-12-20 10:45:32 +0100

    backlog-test-3

---
+++ js/backlog.js
@@ -0,0 +1,322 @@
+
+let assignment = [
+    
+    
+    {
+        'img': 'img/backlog-test.jpg',
+        'name':'Mister Reihmähn',
+        'mailadress':'misterreihmähn@example.com',
+        'category':'produce',
+        'details':'Hier könnte Ihre Werbung stehen'
+},{
+    'img': 'img/backlog-test.jpg',
+    'name':'Mister Reihmähn',
+    'mailadress':'misterreihmähn@example.com',
+    'category':'produce',
+    'details':'Hier könnte Ihre Werbung stehen'
+},{
+    'img': 'img/backlog-test.jpg',
+    'name':'Mister Reihmähn',
+    'mailadress':'misterreihmähn@example.com',
+    'category':'produce',
+    'details':'Hier könnte Ihre Werbung stehen'
+},{
+    'img': 'img/backlog-test.jpg',
+    'name':'Mister Reihmähn',
+    'mailadress':'misterreihmähn@example.com',
+    'category':'produce',
+    'details':'Hier könnte Ihre Werbung stehen'
+},{
+    'img': 'img/backlog-test.jpg',
+    'name':'Mister Reihmähn',
+    'mailadress':'misterreihmähn@example.com',
+    'category':'produce',
+    'details':'Hier könnte Ihre Werbung stehen'
+},{
+    'img': 'img/backlog-test.jpg',
+    'name':'Mister Reihmähn',
+    'mailadress':'misterreihmähn@example.com',
+    'category':'produce',
+    'details':'Hier könnte Ihre Werbung stehen'
+},{
+    'img': 'img/backlog-test.jpg',
+    'name':'Mister Reihmähn',
+    'mailadress':'misterreihmähn@example.com',
+    'category':'produce',
+    'details':'Hier könnte Ihre Werbung stehen'
+},{
+    'img': 'img/backlog-test.jpg',
+    'name':'Mister Reihmähn',
+    'mailadress':'misterreihmähn@example.com',
+    'category':'produce',
+    'details':'Hier könnte Ihre Werbung stehen'
+},{
+    'img': 'img/backlog-test.jpg',
+    'name':'Mister Reihmähn',
+    'mailadress':'misterreihmähn@example.com',
+    'category':'produce',
+    'details':'Hier könnte Ihre Werbung stehen'
+},{
+    'img': 'img/backlog-test.jpg',
+    'name':'Mister Reihmähn',
+    'mailadress':'misterreihmähn@example.com',
+    'category':'produce',
+    'details':'Hier könnte Ihre Werbung stehen'
+},{
+    'img': 'img/backlog-test.jpg',
+    'name':'Mister Reihmähn',
+    'mailadress':'misterreihmähn@example.com',
+    'category':'produce',
+    'details':'Hier könnte Ihre Werbung stehen'
+},{
+    'img': 'img/backlog-test.jpg',
+    'name':'Mister Reihmähn',
+    'mailadress':'misterreihmähn@example.com',
+    'category':'produce',
+    'details':'Hier könnte Ihre Werbung stehen'
+},{
+    'img': 'img/backlog-test.jpg',
+    'name':'Mister Reihmähn',
+    'mailadress':'misterreihmähn@example.com',
+    'category':'produce',
+    'details':'Hier könnte Ihre Werbung stehen'
+},{
+        'img': 'img/backlog-test.jpg',
+        'name':'Mister Reihmähn',
+        'mailadress':'misterreihmähn@example.com',
+        'category':'produce',
+        'details':'Hier könnte Ihre Werbung stehen'
+},{
+    'img': 'img/backlog-test.jpg',
+    'name':'Mister Reihmähn',
+    'mailadress':'misterreihmähn@example.com',
+    'category':'produce',
+    'details':'Hier könnte Ihre Werbung stehen'
+},{
+    'img': 'img/backlog-test.jpg',
+    'name':'Mister Reihmähn',
+    'mailadress':'misterreihmähn@example.com',
+    'category':'produce',
+    'details':'Hier könnte Ihre Werbung stehen'
+},{
+    'img': 'img/backlog-test.jpg',
+    'name':'Mister Reihmähn',
+    'mailadress':'misterreihmähn@example.com',
+    'category':'produce',
+    'details':'Hier könnte Ihre Werbung stehen'
+},{
+    'img': 'img/backlog-test.jpg',
+    'name':'Mister Reihmähn',
+    'mailadress':'misterreihmähn@example.com',
+    'category':'produce',
+    'details':'Hier könnte Ihre Werbung stehen'
+},{
+    'img': 'img/backlog-test.jpg',
+    'name':'Mister Reihmähn',
+    'mailadress':'misterreihmähn@example.com',
+    'category':'produce',
+    'details':'Hier könnte Ihre Werbung stehen'
+},{
+    'img': 'img/backlog-test.jpg',
+    'name':'Mister Reihmähn',
+    'mailadress':'misterreihmähn@example.com',
+    'category':'produce',
+    'details':'Hier könnte Ihre Werbung stehen'
+},{
+    'img': 'img/backlog-test.jpg',
+    'name':'Mister Reihmähn',
+    'mailadress':'misterreihmähn@example.com',
+    'category':'produce',
+    'details':'Hier könnte Ihre Werbung stehen'
+},{
+    'img': 'img/backlog-test.jpg',
+    'name':'Mister Reihmähn',
+    'mailadress':'misterreihmähn@example.com',
+    'category':'produce',
+    'details':'Hier könnte Ihre Werbung stehen'
+},{
+    'img': 'img/backlog-test.jpg',
+    'name':'Mister Reihmähn',
+    'mailadress':'misterreihmähn@example.com',
+    'category':'produce',
+    'details':'Hier könnte Ihre Werbung stehen'
+},{
+    'img': 'img/backlog-test.jpg',
+    'name':'Mister Reihmähn',
+    'mailadress':'misterreihmähn@example.com',
+    'category':'produce',
+    'details':'Hier könnte Ihre Werbung stehen'
+},{
+        'img': 'img/backlog-test.jpg',
+        'name':'Mister Reihmähn',
+        'mailadress':'misterreihmähn@example.com',
+        'category':'produce',
+        'details':'Hier könnte Ihre Werbung stehen'
+},{
+    'img': 'img/backlog-test.jpg',
+    'name':'Mister Reihmähn',
+    'mailadress':'misterreihmähn@example.com',
+    'category':'produce',
+    'details':'Hier könnte Ihre Werbung stehen'
+},{
+    'img': 'img/backlog-test.jpg',
+    'name':'Mister Reihmähn',
+    'mailadress':'misterreihmähn@example.com',
+    'category':'produce',
+    'details':'Hier könnte Ihre Werbung stehen'
+},{
+    'img': 'img/backlog-test.jpg',
+    'name':'Mister Reihmähn',
+    'mailadress':'misterreihmähn@example.com',
+    'category':'produce',
+    'details':'Hier könnte Ihre Werbung stehen'
+},{
+    'img': 'img/backlog-test.jpg',
+    'name':'Mister Reihmähn',
+    'mailadress':'misterreihmähn@example.com',
+    'category':'produce',
+    'details':'Hier könnte Ihre Werbung stehen'
+},{
+    'img': 'img/backlog-test.jpg',
+    'name':'Mister Reihmähn',
+    'mailadress':'misterreihmähn@example.com',
+    'category':'produce',
+    'details':'Hier könnte Ihre Werbung stehen'
+},{
+    'img': 'img/backlog-test.jpg',
+    'name':'Mister Reihmähn',
+    'mailadress':'misterreihmähn@example.com',
+    'category':'produce',
+    'details':'Hier könnte Ihre Werbung stehen'
+},{
+    'img': 'img/backlog-test.jpg',
+    'name':'Mister Reihmähn',
+    'mailadress':'misterreihmähn@example.com',
+    'category':'produce',
+    'details':'Hier könnte Ihre Werbung stehen'
+},{
+    'img': 'img/backlog-test.jpg',
+    'name':'Mister Reihmähn',
+    'mailadress':'misterreihmähn@example.com',
+    'category':'produce',
+    'details':'Hier könnte Ihre Werbung stehen'
+},{
+    'img': 'img/backlog-test.jpg',
+    'name':'Mister Reihmähn',
+    'mailadress':'misterreihmähn@example.com',
+    'category':'produce',
+    'details':'Hier könnte Ihre Werbung stehen'
+},{
+    'img': 'img/backlog-test.jpg',
+    'name':'Mister Reihmähn',
+    'mailadress':'misterreihmähn@example.com',
+    'category':'produce',
+    'details':'Hier könnte Ihre Werbung stehen'
+},{
+    'img': 'img/backlog-test.jpg',
+    'name':'Mister Reihmähn',
+    'mailadress':'misterreihmähn@example.com',
+    'category':'produce',
+    'details':'Hier könnte Ihre Werbung stehen'
+},{
+    'img': 'img/backlog-test.jpg',
+    'name':'Mister Reihmähn',
+    'mailadress':'misterreihmähn@example.com',
+    'category':'produce',
+    'details':'Hier könnte Ihre Werbung stehen'
+},{
+    'img': 'img/backlog-test.jpg',
+    'name':'Mister Reihmähn',
+    'mailadress':'misterreihmähn@example.com',
+    'category':'produce',
+    'details':'Hier könnte Ihre Werbung stehen'
+},{
+    'img': 'img/backlog-test.jpg',
+    'name':'Mister Reihmähn',
+    'mailadress':'misterreihmähn@example.com',
+    'category':'produce',
+    'details':'Hier könnte Ihre Werbung stehen'
+},{
+    'img': 'img/backlog-test.jpg',
+    'name':'Mister Reihmähn',
+    'mailadress':'misterreihmähn@example.com',
+    'category':'produce',
+    'details':'Hier könnte Ihre Werbung stehen'
+},{
+    'img': 'img/backlog-test.jpg',
+    'name':'Mister Reihmähn',
+    'mailadress':'misterreihmähn@example.com',
+    'category':'produce',
+    'details':'Hier könnte Ihre Werbung stehen'
+},{
+    'img': 'img/backlog-test.jpg',
+    'name':'Mister Reihmähn',
+    'mailadress':'misterreihmähn@example.com',
+    'category':'produce',
+    'details':'Hier könnte Ihre Werbung stehen'
+},{
+    'img': 'img/backlog-test.jpg',
+    'name':'Mister Reihmähn',
+    'mailadress':'misterreihmähn@example.com',
+    'category':'produce',
+    'details':'Hier könnte Ihre Werbung stehen'
+},{
+    'img': 'img/backlog-test.jpg',
+    'name':'Mister Reihmähn',
+    'mailadress':'misterreihmähn@example.com',
+    'category':'produce',
+    'details':'Hier könnte Ihre Werbung stehen'
+},{
+    'img': 'img/backlog-test.jpg',
+    'name':'Mister Reihmähn',
+    'mailadress':'misterreihmähn@example.com',
+    'category':'produce',
+    'details':'Hier könnte Ihre Werbung stehen'
+},{
+    'img': 'img/backlog-test.jpg',
+    'name':'Mister Reihmähn',
+    'mailadress':'misterreihmähn@example.com',
+    'category':'produce',
+    'details':'Hier könnte Ihre Werbung stehen'
+},{
+    'img': 'img/backlog-test.jpg',
+    'name':'Mister Reihmähn',
+    'mailadress':'misterreihmähn@example.com',
+    'category':'produce',
+    'details':'Hier könnte Ihre Werbung stehen'
+},{
+    'img': 'img/backlog-test.jpg',
+    'name':'Mister Reihmähn',
+    'mailadress':'misterreihmähn@example.com',
+    'category':'produce',
+    'details':'Hier könnte Ihre Werbung stehen'
+},];
+
+
+
+function backlog() {
+    for (let i = 0; i < assignment.length; i++) {
+
+        document.getElementById('backlog').innerHTML +=`
+
+        <div class="task">
+
+        <div class="assign">
+            <img class="backlog-img" src="${assignment[i].img}">
+            <div class="data">
+                <span>${assignment[i].name}</span>
+                <span style="color:rgb(157, 158, 247)">${assignment[i].mailadress}</span>
+            </div>
+        </div>
+        <div class="category">
+            <span>${assignment[i].category}</span>
+        </div>
+        <div class="details">
+            <span>${assignment[i].details}</span>
+        </div>
+    
+    </div>`;
+        
+        
+    }
+}
